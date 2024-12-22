2cd13c6ec72812ee765c0902093b675695471cec
Julialunna <juliaaluna1@gmail.com>
2024-12-22 15:53:01 -0300

    criando funções requeridas pelo main e corrigindo construtor de cópia

--- polimorfismo/vetor.hpp
+++ polimorfismo/vetor.hpp
@@ -1,13 +1,56 @@
+#include<iostream>
 template <typename T>
+
 class Vetor{
     private:
-        T* vetor_do_tipo_T;
-        int capacidade_maxima_vetor;
+        T* VetorDoTipoT;
+        int CapacidadeMaximaVetor;
+        int NumElementos;
     public:
-        Vetor(int n): capacidade_maxima_vetor(n){
-            vetor_do_tipo_T=new T[capacidade_maxima_vetor];
+        Vetor(Vetor& VetorRecebido): Vetor(VetorRecebido.GetCapacidadeMaximaVetor()){
+            this->CapacidadeMaximaVetor=VetorRecebido.GetCapacidadeMaximaVetor();
+            for(int i=0;i<VetorRecebido.GetNumElementos();i++){
+                this->VetorDoTipoT[i]=VetorRecebido.GetElemento(i);
+                this->NumElementos++;
+            }
+        }
+        Vetor(int n): CapacidadeMaximaVetor(n), NumElementos(0){
+            VetorDoTipoT=new T[CapacidadeMaximaVetor];
+        }
+
+        void SetElemento(int i, T x){
+            if(i<this->CapacidadeMaximaVetor){
+                this->VetorDoTipoT[i]=x;
+            }
+        }
+        T GetElemento(int i){
+            return this->VetorDoTipoT[i];
+        }
+        void AdicionaElemento(T x){
+            if(this->NumElementos<this->CapacidadeMaximaVetor){
+                this->SetElemento(this->NumElementos, x);
+                this->NumElementos++;
+            }
+        }
+        void Imprime(){
+            int i;
+            for(i=0;i<this->NumElementos;i++){
+                if(i==this->NumElementos-1){
+                    std::cout<<this->GetElemento
+                    (i)<<std::endl;
+                }else{
+                    std::cout<<this->GetElemento
+                    (i)<< " "<<std::ends;
+                }
+            }
+        }
+        int GetCapacidadeMaximaVetor(){
+            return this->CapacidadeMaximaVetor;
+        }
+        int GetNumElementos(){
+            return this->NumElementos;
         }
         ~Vetor(){
-            delete[] vetor_do_tipo_T;
+            delete[] VetorDoTipoT;
         }
 };
